3c1ac665a14c45d4882768dfb34cfac416a4025c
Joshua Ulimer Demerin <joshuaulimer.demerin-20@cpu.edu.ph>
2024-09-30 15:05:23 +0800

    prelims commit

---
+++ script.js
@@ -0,0 +1,54 @@
+let display = document.getElementById('display');
+let isOn = true;
+
+const language = "Hello!";
+
+function appendToDisplay(value) {
+    if (isOn) {
+        const lastChar = display.value[display.value.length - 1];
+        const isOperator = ['+', '-', '*', '/'].includes(value);
+        const hasOperator = ['+', '-', '*', '/'].includes(lastChar);
+        
+        if (isOperator && hasOperator) return; // Tjis will prevent multiple operators.
+        if (value === '.' && lastChar === '.') return; // And this will prevent multiple decimal points.
+        if (display.value.length < 8) {
+            display.value += value;
+        }
+    }
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function backspace() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    if (isOn) {
+        try {
+            display.value = eval(display.value);
+        } catch {
+            display.value = 'Error';
+        }
+    }
+}
+
+function turnOff() {
+    isOn = false;
+    display.value = 'Goodbye!';
+    setTimeout(() => {
+        display.value = '';
+        isOn = true;
+    }, 2000);
+}
+
+function showHello() {
+    if (isOn) {
+        display.value = language;
+        setTimeout(() => {
+            display.value = '';
+        }, 2000);
+    }
+}
